cf6ff055f6d4adee1d0ee875af90e0551f0fbe99
jerrylin <iii20220906linjerry@gmail.com>
2024-03-27 16:36:34 +0800

    feat:輪播banner

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,13 +1,44 @@
+<script setup>
+import { getBannerAPI } from '@/apis/home.js'
+import { ref, onMounted } from 'vue'
+
+const bannerList = ref([])
+const getBanner = async () => {
+    const { data: { result } } = await getBannerAPI()
+    console.log(result);
+    bannerList.value = result
+}
+onMounted(() => {
+    getBanner()
+});
+</script>
+
+
+
 <template>
-    <div>
-        banner
+    <div class="home-banner">
+        <el-carousel height="500px">
+            <el-carousel-item v-for="item in bannerList" :key="item">
+                <img :src="item.imgUrl" alt="">
+            </el-carousel-item>
+        </el-carousel>
     </div>
 </template>
 
-<script setup>
 
-</script>
 
-<style lang="scss" scoped>
+<style scoped lang='scss'>
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 98;
 
+    img {
+        width: 100%;
+        height: 500px;
+    }
+}
 </style>
