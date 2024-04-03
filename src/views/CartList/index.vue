f9c572bf0507326f383126c370710fbac46b5820
jerrylin <iii20220906linjerry@gmail.com>
2024-04-03 16:55:20 +0800

    feat:86

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -1,7 +1,14 @@
 <script setup>
 
 import { useCartStore } from '@/stores/cart';
-    const cartStore = useCartStore();
+const cartStore = useCartStore();
+const singleCheck = (i, selected) => {
+  cartStore.singleCheck(i.skuId, selected)
+
+};
+const allCheck = (selected)=>{
+  cartStore.allCheck(selected)
+};
 </script>
 
 <template>
@@ -12,7 +19,7 @@ import { useCartStore } from '@/stores/cart';
           <thead>
             <tr>
               <th width="120">
-                <el-checkbox/>
+                <el-checkbox :model-value="cartStore.isAll" @change="allCheck"  />
               </th>
               <th width="400">商品信息</th>
               <th width="220">单价</th>
@@ -25,7 +32,8 @@ import { useCartStore } from '@/stores/cart';
           <tbody>
             <tr v-for="i in cartStore.cartList" :key="i.id">
               <td>
-                <el-checkbox />
+                <!-- 單選 -->
+                <el-checkbox :model-value="i.selected" @change="(selected) => singleCheck(i, selected)" />
               </td>
               <td>
                 <div class="goods">
@@ -72,11 +80,11 @@ import { useCartStore } from '@/stores/cart';
       <!-- 操作栏 -->
       <div class="action">
         <div class="batch">
-          共 10 件商品，已选择 2 件，商品合计：
-          <span class="red">¥ 200.00 </span>
+          共 {{ cartStore.allCount }} 件商品，已选择 {{ cartStore.selectedCount }} 件，商品合计：
+          <span class="red">¥ {{cartStore.selectedPrice.toFixed(2)}} </span>
         </div>
         <div class="total">
-          <el-button size="large" type="primary" >下单结算</el-button>
+          <el-button size="large" type="primary">下单结算</el-button>
         </div>
       </div>
     </div>
